b5da70889f2caeabec80be5da8a54cf5647994cb
samuelds <samuelds.pro@gmail.com>
2017-08-13 12:32:51 +0200

    Update function name

--- src/player.client.lua
+++ src/player.client.lua
@@ -7,25 +7,36 @@
 
 Player = {}
 
-function GetPlayer()
-  return Player
-end
-
--- Get value by key
-function Get(name)
-  return Player[name]
-end
-
--- Set value
-function Set(..)
+function GetPlayer(..)
 
   local arg = {...}
 
   if #arg == 1 and type(arg[1]) == "table" then
 
-  for name, value in pairs(data) do
-    Player[name] = value
+    local table = {}
+    for _, name in pairs(arg[1]) do
+      table[name] = Player[name]
+    end
+
+  elseif #arg == 1 then
+
+    local name = arg[1]
+    return Player[name]
+
   end
+
+end
+
+-- Set value
+function SetPlayer(..)
+
+  local arg = {...}
+
+  if #arg == 1 and type(arg[1]) == "table" then
+
+    for name, value in pairs(arg[1]) do
+      Player[name] = value
+    end
 
   elseif #arg == 2 then
 
