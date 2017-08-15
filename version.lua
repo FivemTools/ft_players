f11a0aedcbe03029d26c3e9771a171e34669869b
samuelds <samuelds.pro@gmail.com>
2017-08-15 15:37:50 +0200

    Add header

--- version.lua
+++ version.lua
@@ -1,1 +1,8 @@
-version = "0.1.0"
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+-- @Source: https://github.com/FivemTools/ft_players
+--
+
+Version = "0.1.0"
